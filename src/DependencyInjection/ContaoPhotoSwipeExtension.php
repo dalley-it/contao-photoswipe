2d21f206c22dda7b409c5ce3535f6195ce26edaa
Dalley IT <info@dalley-it.com>
2019-02-02 21:35:22 +0100

    Update ContaoPhotoSwipeExtension.php

--- src/DependencyInjection/ContaoPhotoSwipeExtension.php
+++ src/DependencyInjection/ContaoPhotoSwipeExtension.php
@@ -25,17 +25,20 @@
  
 namespace Dait\ContaoPhotoSwipe\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
 
 class ContaoPhotoSwipeExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $mergedConfig, ContainerBuilder $container)
     {
-		protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
-		{
-			$loader = new YamlFileLoader($container, new FileLocator(__DIR__.’/../Resources/config’));
-			$loader->load(’services.yml’);
-		}
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
     }
 }
